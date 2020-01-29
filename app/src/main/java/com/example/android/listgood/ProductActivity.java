906bf2929218706ec5217a38ab519535730cae0e
lemaitre <lemaitre@bk.ru>
2020-01-29 15:27:49 +1000

    pause

--- app/src/main/java/com/example/android/listgood/ProductActivity.java
+++ app/src/main/java/com/example/android/listgood/ProductActivity.java
@@ -3,12 +3,24 @@ package com.example.android.listgood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ProductActivity extends AppCompatActivity {
+    String[] arrayStrings;
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        arrayStrings = new String[]{"мясо", "молоко", "сахар"};
+        listView = findViewById(R.id.listViewProduct);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
